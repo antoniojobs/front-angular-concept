67d2fa1508e294bbe9f351469777eba6724d5431
José Antonio <br014801398740@enel.com>
2022-05-23 15:18:13 -0300

    start

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { ClientesFormComponent } from "./clientes/clientes-form/clientes-form.component";
+
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'clientes-form',component:ClientesFormComponent},
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './home/home.component'
+import { ClientesModule } from './clientes/clientes.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    TemplateModule,
+    ClientesModule,
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
